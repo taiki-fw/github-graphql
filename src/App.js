cd51796d890a9fae1ded1b667705f2916c00a12c
taiki-fw <taiki_52@outlook.jp>
2020-02-25 22:33:05 +0900

    feat:repositoryをstar・unstar出来る機能の追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
-import React, { Component } from "react";
-import { Query } from "react-apollo";
+import React from "react";
+import { Query, Mutation } from "react-apollo";
 import gql from "graphql-tag";
+
+const ADD_STAR_REPOSITORY = gql`
+  mutation($id: ID!) {
+    addStar(input: { starrableId: $id }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
+const REMOVE_STAR_REPOSITORY = gql`
+  mutation($id: ID!) {
+    removeStar(input: { starrableId: $id }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
 
 const query = gql`
   {
@@ -33,6 +55,26 @@ const App = () => (
             <li key={repo.id}>
               <a href={repo.url}>{repo.name}</a>
               <button>{repo.stargazers.totalCount} Star</button>
+
+              {!repo.viewerHasStarred ? (
+                <Mutation
+                  mutation={ADD_STAR_REPOSITORY}
+                  variables={{ id: repo.id }}
+                >
+                  {(addStar, { data, loading, error }) => (
+                    <button onClick={addStar}>star</button>
+                  )}
+                </Mutation>
+              ) : (
+                <Mutation
+                  mutation={REMOVE_STAR_REPOSITORY}
+                  variables={{ id: repo.id }}
+                >
+                  {(removeStar, { data, loading, error }) => (
+                    <button onClick={removeStar}>unstar</button>
+                  )}
+                </Mutation>
+              )}
             </li>
           ))}
         </ul>
